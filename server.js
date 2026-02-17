8431efa1eb6439e237671d23d401dde235783dc8
Adityayadav8957 <adityayadav7868@gmail.com>
2026-02-18 02:00:26 +0530

    cors

--- server.js
+++ server.js
@@ -14,8 +14,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+    'http://localhost:5174',
+    'https://adio-frontend.vercel.app'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+})); app.use(express.json());
 
 // Routes
 app.use('/api', routes);
